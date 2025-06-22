336fcf058ae97f6af50025fd65c446ac7628eff5
Pritam Som <pritamsom334@gmail.com>
2025-06-22 15:06:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,11 @@ function opentab(tabname){
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
+ document.addEventListener("DOMContentLoaded", function () {
+  const toggle = document.getElementById("menu-toggle");
+  const navList = document.getElementById("nav-list");
+
+  toggle.addEventListener("click", () => {
+    navList.classList.toggle("show");
+  });
+});
